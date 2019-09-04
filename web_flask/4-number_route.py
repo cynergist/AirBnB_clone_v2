1b3b3c1c587db4bc13363c99cab01e772f58597a
Cyn <cmdominguez@gmail.com>
2019-09-04 04:01:07 +0000

    add 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+''' Script starts a Flask web application '''
+
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def home():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is(text='is cool'):
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.debug = True
+    # Don't run on localhost
+    app.run(host='0.0.0.0', port=5000)
