53bda3a8809d71bb4317642ef599278c96928528
Cyn <cmdominguez@gmail.com>
2019-09-07 22:18:44 +0000

    add web_flask/8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,8 +15,10 @@ def states_list():
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     ''' renders template for jinja cities_by_states list '''
+    states = storage.all('State')
     cities = storage.all('City')
-    return render_template('8-cities_by_states.html', cities=cities)
+    return render_template('8-cities_by_states.html',
+                           states=states, cities=cities)
 
 
 @app.teardown_appcontext
